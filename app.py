4870d88539ab9b83cac74a814321702279d6f6c5
Deris01 <derysamaulana@gmail.com>
2025-12-11 20:25:11 +0700

    Update

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def main():
         /* 3. Tombol (BUTTONS) */
         .stButton > button {
             font-family: 'Montserrat', sans-serif !important;
-            font-weight: 200 !important; 
+            font-weight: 600 !important; 
             font-size: 16px !important;
             text-transform: uppercase !important; 
             letter-spacing: 1px !important; 
@@ -202,6 +202,7 @@ def main():
 if __name__ == '__main__':
 
     main()
+
 
 
 
